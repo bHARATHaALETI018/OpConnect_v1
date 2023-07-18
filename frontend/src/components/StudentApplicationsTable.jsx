2c08172b9f867fcd67b0378f31f636d911560d8f
BHARATHaALETI018 <bharathaaleti018@gmail.com>
2023-07-18 17:19:12 +0530

    minor changes

--- frontend/src/components/StudentApplicationsTable.jsx
+++ frontend/src/components/StudentApplicationsTable.jsx
@@ -127,7 +127,7 @@ const StudentApplicationsTable = (props) => {
           className="flex-auto m-12 rounded-2xl bg-third m-3 p-9 flex flex-col font-bold text-xl items-center
         transition ease-in-out bg-blue-500 hover:translate-y-1 hover:scale-110 hover:bg-indigo-500 "
         >
-          <p className="capitalize">app. in Diff. companies</p>
+          <p className="capitalize">Opp. in Diff. companies</p>
           <p className="text-4xl">{Companies.size}</p>
         </div>
         <div
@@ -145,118 +145,129 @@ const StudentApplicationsTable = (props) => {
           <p className="text-4xl">{rejected.length}</p>
         </div>
       </div>
-      <div>
-        <h1 className="text-3xl font-bold text-center text-fourth underline underline-offset-4 decoration-white/[0.33] decoration-2 decoration-wavy  mb-4">
-          Applications
-        </h1>
-        <div className="flex justify-center gap-3 font-semibold">
-          <div className="flex flex-col gap-1 items-center justify-center my-4 ">
-            <label htmlFor="appNumberFilter" className="mr-2">
-              Application Number:
-            </label>
-            <input
-              type="text"
-              id="appNumberFilter"
-              placeholder="search by id"
-              value={appNumberFilter}
-              onChange={(e) => setAppNumberFilter(e.target.value)}
-              className="border border-second border-4 text-white px-4 py-2 rounded-xl"
-              style={{ width: "12rem", backgroundColor: "#282828" }}
-            />
-          </div>
-          <div className="flex flex-col gap-1 items-center justify-center my-4">
-            <label htmlFor="titleFilter" className="mr-2">
-              Title:
-            </label>
-            <input
-              type="text"
-              id="titleFilter"
-              placeholder="search by Title"
-              value={titleFilter}
-              onChange={(e) => setTitleFilter(e.target.value)}
-              className="border border-second border-4 text-white  px-4 py-2 rounded-xl"
-              style={{ backgroundColor: "#282828" }}
-            />
-          </div>
-          <div className="flex flex-col gap-1 items-center justify-center my-4">
-            <label htmlFor="companyFilter" className="mr-2">
-              Company:
-            </label>
-            <input
-              type="text"
-              id="companyFilter"
-              placeholder="search by company"
-              value={companyFilter}
-              onChange={(e) => setCompanyFilter(e.target.value)}
-              className="border border-second border-4 text-white  px-4 py-2 rounded-xl"
-              style={{ backgroundColor: "#282828" }}
-            />
-          </div>
-          <div className="flex flex-col gap-1 items-center justify-center my-4">
-            <label htmlFor="statusFilter" className="mr-2">
-              Status:
-            </label>
-            <select
-              id="statusFilter"
-              value={statusFilter}
-              onChange={(e) => setStatusFilter(e.target.value)}
-              className="border border-second border-4 text-white px-4 py-2 rounded-xl"
-              style={{ backgroundColor: "#190061" }}
-            >
-              <option value="">All</option>
-              <option value="Applied">Applied</option>
-              <option value="Accepted">Accepted</option>
-              <option value="Rejected">Rejected</option>
-            </select>
-          </div>
-          <button
-            onClick={(e) => handleFilter(e)}
-            className="border bg-fourth border-second border-4 px-4 py-2 rounded-xl"
-            disabled={loading}
-          >
-            Apply
-            {loading && (
-              <div className="flex justify-center mt-2">
-                <Oval color="#ffffff" height={20} width={20} />
-              </div>
-            )}
-          </button>
+
+      {applications.length === 0 ? (
+        <div className="flex flex-col gap-3 text-center my-32 text-2xl font-semibold">
+          <h1>Opps...</h1> <h1>You don't have any applications.</h1>
         </div>
-        <table className="mt-4 border-collapse w-full bg-first">
-          <thead>
-            <tr className="bg-fourth">
-              <th className="border px-4 py-2">Opportunity ID</th>
-              <th className="border px-4 py-2">Title</th>
-              <th className="border px-4 py-2">Company</th>
-              <th className="border px-4 py-2">Description</th>
-              <th className="border px-4 py-2">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {applications.map((application) => (
-              <tr key={application._id}>
-                <td className="border px-4 py-2">{application.appNumber}</td>
-                <td className="border px-4 py-2">{application.title}</td>
-                <td className="border px-4 py-2">{application.companyName}</td>
-                <td className="border px-4 py-2">{application.description}</td>
-                <td className="border px-4 py-2">
-                  <select
-                    value={application.status}
-                    onChange={(e) =>
-                      handleStatusChange(application._id, e.target.value)
-                    }
-                    className="px-4 py-2 rounded-xl bg-fifth p-2 border border-4 border-fourth hover:bg-fourth"
-                  >
-                    <option value="Applied">Applied</option>
-                    <option value="Accepted">Accepted</option>
-                    <option value="Rejected">Rejected</option>
-                  </select>
-                </td>
+      ) : (
+        <div>
+          <h1 className="text-3xl font-bold text-center text-fourth underline underline-offset-4 decoration-white/[0.33] decoration-2 decoration-wavy  mb-4">
+            Applications
+          </h1>
+          <div className="flex justify-center gap-3 font-semibold">
+            <div className="flex flex-col gap-1 items-center justify-center my-4 ">
+              <label htmlFor="appNumberFilter" className="mr-2">
+                Application Number:
+              </label>
+              <input
+                type="text"
+                id="appNumberFilter"
+                placeholder="search by id"
+                value={appNumberFilter}
+                onChange={(e) => setAppNumberFilter(e.target.value)}
+                className="border border-second border-4 text-white px-4 py-2 rounded-xl"
+                style={{ width: "12rem", backgroundColor: "#282828" }}
+              />
+            </div>
+            <div className="flex flex-col gap-1 items-center justify-center my-4">
+              <label htmlFor="titleFilter" className="mr-2">
+                Title:
+              </label>
+              <input
+                type="text"
+                id="titleFilter"
+                placeholder="search by Title"
+                value={titleFilter}
+                onChange={(e) => setTitleFilter(e.target.value)}
+                className="border border-second border-4 text-white  px-4 py-2 rounded-xl"
+                style={{ backgroundColor: "#282828" }}
+              />
+            </div>
+            <div className="flex flex-col gap-1 items-center justify-center my-4">
+              <label htmlFor="companyFilter" className="mr-2">
+                Company:
+              </label>
+              <input
+                type="text"
+                id="companyFilter"
+                placeholder="search by company"
+                value={companyFilter}
+                onChange={(e) => setCompanyFilter(e.target.value)}
+                className="border border-second border-4 text-white  px-4 py-2 rounded-xl"
+                style={{ backgroundColor: "#282828" }}
+              />
+            </div>
+            <div className="flex flex-col gap-1 items-center justify-center my-4">
+              <label htmlFor="statusFilter" className="mr-2">
+                Status:
+              </label>
+              <select
+                id="statusFilter"
+                value={statusFilter}
+                onChange={(e) => setStatusFilter(e.target.value)}
+                className="border border-second border-4 text-white px-4 py-2 rounded-xl"
+                style={{ backgroundColor: "#190061" }}
+              >
+                <option value="">All</option>
+                <option value="Applied">Applied</option>
+                <option value="Accepted">Accepted</option>
+                <option value="Rejected">Rejected</option>
+              </select>
+            </div>
+            <button
+              onClick={(e) => handleFilter(e)}
+              className="border bg-fourth border-second border-4 px-4 py-2 rounded-xl"
+              disabled={loading}
+            >
+              Apply
+              {loading && (
+                <div className="flex justify-center mt-2">
+                  <Oval color="#ffffff" height={20} width={20} />
+                </div>
+              )}
+            </button>
+          </div>
+          <table className="mt-4 border-collapse w-full bg-first">
+            <thead>
+              <tr className="bg-fourth">
+                <th className="border px-4 py-2">Opportunity ID</th>
+                <th className="border px-4 py-2">Title</th>
+                <th className="border px-4 py-2">Company</th>
+                <th className="border px-4 py-2">Description</th>
+                <th className="border px-4 py-2">Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {applications.map((application) => (
+                <tr key={application._id}>
+                  <td className="border px-4 py-2">{application.appNumber}</td>
+                  <td className="border px-4 py-2">{application.title}</td>
+                  <td className="border px-4 py-2">
+                    {application.companyName}
+                  </td>
+                  <td className="border px-4 py-2">
+                    {application.description}
+                  </td>
+                  <td className="border px-4 py-2">
+                    <select
+                      value={application.status}
+                      onChange={(e) =>
+                        handleStatusChange(application._id, e.target.value)
+                      }
+                      className="px-4 py-2 rounded-xl bg-fifth p-2 border border-4 border-fourth hover:bg-fourth"
+                    >
+                      <option value="Applied">Applied</option>
+                      <option value="Accepted">Accepted</option>
+                      <option value="Rejected">Rejected</option>
+                    </select>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
