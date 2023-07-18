13cdc3679fe324184fb3ca45953bb8e708cd9c59
BHARATHaALETI018 <bharathaaleti018@gmail.com>
2023-07-18 16:12:51 +0530

    added Developer name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <div className="w-full text-center font-bold text-3xl p-6 bg-[#6A0DAD] top-0 sticky border-b border-b-2">
         AppMonitor{" "}
         <span className="rounded-full absolute capitalize top-0 text-black outline outline-indigo-500 opacity-75 right-0 p-3 m-2 text-sm flex flex-col  bg-slate-100">
-          <spam className="font-semibold">creater</spam>
+          <spam className="font-semibold">Developer</spam>
           <a
             href={"https://bharathaaleti018.netlify.app/"}
             target="_blank"
